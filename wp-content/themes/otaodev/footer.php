24c063b2ac859324ac649fb2c7e089eed68fec42
Tiago Oliveira <contato@otao.dev.br>
2025-06-25 13:38:59 -0300

    Remove unused template files and theme configuration for Twenty Twenty-Three theme

--- wp-content/themes/otaodev/footer.php
+++
@@ -1,8 +0,0 @@
-<footer id="colophon" class="site-footer footer">
-    <div class="site-info">
-        <?php printf(esc_html__('© %s - All rights reserved', 'seu-tema'), get_bloginfo('name')); ?>
-    </div>
-</footer>
-<?php wp_footer(); ?>
-</body>
-</html>
